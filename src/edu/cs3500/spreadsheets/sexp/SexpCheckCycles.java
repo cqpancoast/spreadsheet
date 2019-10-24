e3440c9b89d84d5400b18d961dfbe619eb72aeca
angelagross <gross.angela.c@gmail.com>
2019-10-23 21:31:29 -0400

    hopefully the cycle checker works

--- src/edu/cs3500/spreadsheets/sexp/SexpCheckCycles.java
+++ src/edu/cs3500/spreadsheets/sexp/SexpCheckCycles.java
@@ -2,11 +2,25 @@ package edu.cs3500.spreadsheets.sexp;
 
 import java.util.List;
 
+/**
+ * The sole field of this class is an {@link Sexp} that is the parsed name of the Coord whose
+ * formula is being checked for cycles.
+ *
+ * This {@link SexpVisitor} determines whether the {@link Sexp} of the Coord in question is found
+ * in its formula's own {@link Sexp}, either directly or indirectly, which would create a cycle.
+ *
+ * The {@link SexpVisitor} returns true if there are cycles, and false if there are not cycles.
+ */
 public class SexpCheckCycles implements SexpVisitor<Boolean> {
-  private final Sexp symbol;
+  private final Sexp sexp;
 
-  public SexpCheckCycles(Sexp symbol) {
-    this.symbol = symbol;
+  /**
+   * Constructs an {@link SexpVisitor} that checks for cycles in the {@link Sexp} of the given cell.
+   *
+   * @param sexp   the name of the cell in question as an {@link Sexp}
+   */
+  public SexpCheckCycles(Sexp sexp) {
+    this.sexp = sexp;
   }
 
   @Override
@@ -21,8 +35,8 @@ public class SexpCheckCycles implements SexpVisitor<Boolean> {
 
   @Override
   public Boolean visitSList(List<Sexp> l) {
-    for (Sexp sexp : l) {
-      if (sexp.accept(new SexpCheckCycles(symbol))) {
+    for (Sexp each : l) {
+      if (each.accept(new SexpCheckCycles(sexp))) {
         return true;
       }
     }
@@ -31,7 +45,10 @@ public class SexpCheckCycles implements SexpVisitor<Boolean> {
 
   @Override
   public Boolean visitSymbol(String s) {
-    return false; // TODO
+    if (Parser.parse(s).equals(sexp)) {
+      return true;
+    }
+    return Parser.parse(s).accept(new SexpCheckCycles(sexp));
   }
 
   @Override
